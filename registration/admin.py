e0b96bb10419547af7b9862f6257f423c3f6308c
Matheus <mggcmatheus@gmail.com>
2022-12-15 19:06:04 -0400

    .

--- registration/admin.py
+++ registration/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Ticker
+from .models import *
 
 
 @admin.register(Category)
@@ -16,3 +16,16 @@ class TickerAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'code', 'category_id', 'is_active']
     list_display_links = ['id', 'name']
     list_per_page = 20
+
+
+@admin.register(AssetWallet)
+class AssetWalletAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = ['id', 'name', 'is_active']
+
+
+@admin.register(AssetsInWallet)
+class AssetWalletInAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = ['id', 'asset_wallet', 'ticker']
+    raw_id_fields = ['ticker']
