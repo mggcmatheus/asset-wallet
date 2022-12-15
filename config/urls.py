22ce5e6f7cde35d1dcc11351e3540f256938d744
Matheus <mggcmatheus@gmail.com>
2022-12-15 17:34:50 -0400

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from registration.api import api as registration_api
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('registration/', registration_api.urls)
+    path('registration/', include('registration.urls'))
 ]
